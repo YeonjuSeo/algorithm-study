59d8eedc503382f81f6235d0ec0a6139c0364b45
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-12-14 20:19:19 +0900

    1978, 2438, 2439, 2446, 252, 2588 풀이

---
+++ 2438/2438.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+}
---
+++ 2446/2446.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0);
+	int N;
+	cin >> N;
+	for (int i = 0; i <N-1; i++) {
+		for (int j = 0; j < i; j++) {
+			cout << ' ';
+		}
+		for (int j = 0; j < (2 *(N - i) - 1); j++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+	for (int i = 0; i < N; i++) {
+		for (int j = N - i; j > 1; j--) {
+			cout << ' ';
+		}
+		for (int j = 0; j < (2*i+1); j++) {
+			cout << '*';
+		}
+		cout << '\n';
+	}
+}
