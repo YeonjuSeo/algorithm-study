8372c01cf9d68f144242a8ff3cf1b8c9fe35ac51
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2021-02-28 21:42:21 +0900

    Create I.cpp

---
+++ c++/2021_Winter_SUAPC/I.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int N;
+char start = 'a';
+string str;
+
+int main() {
+	ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cout << 'a';
+	}
+}
